164f914ec0b70ff1445342e60246f3ab8cc1b60c
Lostflopp <85348206+Lostflopp@users.noreply.github.com>
2023-06-25 19:11:28 +0200

    Assignmen1
    Moved around some code to make it easier to read and reuse.

--- src/test/java/com/infrasight/kodtest/Tests.java
+++ src/test/java/com/infrasight/kodtest/Tests.java
@@ -2,7 +2,9 @@ package com.infrasight.kodtest;
 
 
 import okhttp3.*;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -19,11 +21,51 @@ import static org.junit.Assert.assertTrue;
  */
 public class Tests extends TestsSetup {
 
+	OkHttpClient client = new OkHttpClient().newBuilder().build();
+	MediaType mediaType = MediaType.parse("application/json");
+	Request request;
+	RequestBody body;
+	Response response;
+	String responseBody;
+	JSONObject jsonResponse;
+	JSONArray jsonArray;
+	JSONObject jsonObject;
+	String param;
+	String token;
+
 	String apiURL = "http://localhost:" + TestVariables.API_PORT + "/api/";
+	int verasID = 1337;
+
+	int status;
+	String URL;
 
 	/**
 	 * Simple example test which asserts that the Kodtest API is up and running.
 	 */
+	@Before
+	public void getToken() throws InterruptedException, IOException {
+		URL = apiURL + "auth";
+
+		body = RequestBody.create(mediaType, "{\r\n  \"user\": \"apiUser\",\r\n  \"password\": \"apiPassword!\"\r\n}");
+		request = new Request.Builder()
+				.url(URL)
+				.method("POST", body)
+				.addHeader("Content-Type", "application/json")
+				.build();
+		response = client.newCall(request).execute();
+		status = response.code();
+
+		// Get the response body as a JSON object
+		responseBody = response.body().string();
+		jsonResponse  = new JSONObject(responseBody);
+
+		// Extract the value
+		token = jsonResponse.getString("token");
+		//System.out.println("This is the token" + token);
+
+		assertEquals(200, status);
+
+	}
 
 	@Test
 	public void connectionTest() throws InterruptedException {
@@ -32,65 +74,57 @@ public class Tests extends TestsSetup {
 
 	@Test
 	public void assignment1() throws InterruptedException, IOException {
-		// Create the URL object
+		/**
+		 * TODO: Add code to solve the first assignment. Add Assert to show that you
+		 * found the account for Vera
+		 */
 
-		String URL = apiURL + "auth";
-
-
-		OkHttpClient client = new OkHttpClient().newBuilder().build();
-
-		MediaType mediaType = MediaType.parse("application/json");
-		RequestBody body = RequestBody.create(mediaType, "{\r\n  \"user\": \"apiUser\",\r\n  \"password\": \"apiPassword!\"\r\n}");
-		Request request = new Request.Builder()
-				.url(URL)
-				.method("POST", body)
-				.addHeader("Content-Type", "application/json")
-				.build();
-		Response response = client.newCall(request).execute();
-		long status = response.code();
-
-		// Get the response body as a JSON object
-		String responseBody = response.body().string();
-		JSONObject jsonResponse  = new JSONObject(responseBody);
-
-		// Extract the value
-		String token = jsonResponse.getString("token");
-
-
-		System.out.println("This is the token" + token);
-		assertEquals(200, status);
-
-		String param ="?filter=employeeId%3D1337";
+		param ="?filter=employeeId="+verasID;
 		URL = apiURL + "accounts" + param;
-
-
-		client = new OkHttpClient().newBuilder().build();
 
 		request = new Request.Builder()
 				.url(URL)
 				.method("GET",null)
-				.addHeader("Authorization", "Bearer "+token)
+				.addHeader("Authorization", "Bearer "+ token)
 				.build();
 		response = client.newCall(request).execute();
 		status = response.code();
 
-		System.out.println("This is the body" + response.body().string());
 		assertEquals(200, status);
+
+		// Parse the JSON string as a JSONArray
+		jsonArray = new JSONArray(response.body().string());
+
+		// Extract the first JSONObject from the JSONArray
+		jsonObject = jsonArray.getJSONObject(0);
+
+		// Get the response body as a JSON object
+		responseBody = jsonObject.toString();
+		jsonResponse  = new JSONObject(responseBody);
+
+		// Extract the value
+		int employeeId = jsonResponse.getInt("employeeId");
+
+		//System.out.println("This is the employeeId " + employeeId);
+
+		// Verifying correct account for Vera
+		assertEquals(verasID, employeeId);
 
 	}
 
 	@Test
 	public void assignment2() throws InterruptedException {
 		assertTrue(serverUp);
-
 		/**
 		 * TODO: Add code to solve the second assignment where we expect the number of
 		 * groups to be 3.
 		 */
+		
+
 		int groupCount = 3;
 
 		// Assert which verifies the expected group count of 3
-		assertEquals(3, groupCount);
+		assertEquals(groupCount, 3);
 
 		/**
 		 * TODO: Add Assert to verify the IDs of the groups found
