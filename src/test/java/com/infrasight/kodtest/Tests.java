d23ebc1342b3eb430d30ff65432f773e93a44ca6
Lostflopp <85348206+Lostflopp@users.noreply.github.com>
2023-06-27 23:06:43 +0200

    Assignment3 + Updates
    minimized the code.
    Assignment3 is up and running with the new code

--- src/test/java/com/infrasight/kodtest/Tests.java
+++ src/test/java/com/infrasight/kodtest/Tests.java
@@ -5,10 +5,13 @@ import okhttp3.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -29,16 +32,114 @@ public class Tests extends TestsSetup {
 	Response response;
 	String responseBody;
 	JSONObject jsonResponse;
-	JSONArray jsonArray;
+	JSONArray groupArray;
+	JSONArray relationshipsArray;
+	JSONArray accountArray;
 	JSONObject jsonObject;
-	String param;
+	List<JSONObject> accObjects = new ArrayList<>();
+	List<JSONObject> groupObjects = new ArrayList<>();
+	List<JSONObject> relationshipsObjects = new ArrayList<>();
 	String token;
-
 	String apiURL = "http://localhost:" + TestVariables.API_PORT + "/api/";
-	int verasEmployeeId = 1337;
-	String verasID;
 	int status;
 	String URL;
+	JSONObject verasAcc;
+	String verasEmployeeId = "1337";
+	String verasID;
+
+
+	public Response getRequest(String param ) {
+
+		try {
+
+			URL = apiURL + param;
+			request = new Request.Builder()
+					.url(URL)
+					.method("GET",null)
+					.addHeader("Authorization", "Bearer "+ getToken())
+					.build();
+			response = client.newCall(request).execute();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			// Handle the exception as desired (e.g., logging, throwing custom exception, etc.)
+		}
+		return response;
+	}
+
+	//Get token
+	public String getToken() throws IOException  {
+
+		if (token == null || token.length() == 0) {
+			try {
+				URL = apiURL + "auth";
+
+				body = RequestBody.create(mediaType, "{\r\n  \"user\": \"apiUser\",\r\n  \"password\": \"apiPassword!\"\r\n}");
+				request = new Request.Builder()
+						.url(URL)
+						.method("POST", body)
+						.addHeader("Content-Type", "application/json")
+						.build();
+				response = client.newCall(request).execute();
+				status = response.code();
+
+				// Get the response body as a JSON object
+				responseBody = response.body().string();
+				jsonResponse = new JSONObject(responseBody);
+
+				// Extract the value
+				token = jsonResponse.getString("token");
+			} catch (Exception e) {
+				// Handle the exception (e.g., log error, throw custom exception, etc.)
+				e.printStackTrace();
+			}
+		}
+		return token;
+
+	}
+
+	public JSONObject getAccWithEmployeeId(String employeeId) throws IOException {
+		boolean containsID = false;
+		for (JSONObject obj : accObjects) {
+			if (obj.has("employeeId") && obj.getString("employeeId") == employeeId) {
+				containsID = true;
+				return obj;
+			}
+		}
+		if (!containsID) {
+			String param = "accounts?filter=employeeId=" + employeeId;
+			try {
+				getRequest(param);
+
+				// Parse the JSON string as a JSONArray
+				accountArray = new JSONArray(response.body().string());
+
+				if (accountArray.length() > 0) {
+					// Extract the first JSONObject from the JSONArray
+					JSONObject jsonObject = accountArray.getJSONObject(0);
+
+					accObjects.add(jsonObject);
+
+					return jsonObject;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	public JSONArray getMemberGroupRelationWithId(String memberId) throws IOException {
+		String param = "relationships?filter=memberId=" + memberId;
+			getRequest(param);
+
+			// Parse the JSON string as a JSONArray
+			relationshipsArray = new JSONArray(response.body().string());
+
+				return relationshipsArray;
+	}
+
+
 
 	/**
 	 * Simple example test which asserts that the Kodtest API is up and running.
@@ -47,46 +148,24 @@ public class Tests extends TestsSetup {
 
 	@Test
 	public void connectionTest() throws InterruptedException {
-		System.out.println("NOW RUNNING ConnectionTest!");
 		assertTrue(serverUp);
 	}
 
 	@Test
 	public void assignment1() throws InterruptedException, IOException {
+		assertTrue(serverUp);
+
+		verasAcc = getAccWithEmployeeId(verasEmployeeId);
+
+		// Extract the value
+		verasID = verasAcc.getString("id");
+		String employeeId = verasAcc.getString("employeeId");
+
+
 		/**
 		 * TODO: Add code to solve the first assignment. Add Assert to show that you
 		 * found the account for Vera
 		 */
-		System.out.println("NOW RUNNING ASSIGNMENT1!");
-		param ="?filter=employeeId="+verasEmployeeId;
-		URL = apiURL + "accounts" + param;
-
-		request = new Request.Builder()
-				.url(URL)
-				.method("GET",null)
-				.addHeader("Authorization", "Bearer "+ token)
-				.build();
-		response = client.newCall(request).execute();
-
-		//check HTTP response status codes
-		status = response.code();
-		assertEquals(200, status);
-
-		// Parse the JSON string as a JSONArray
-		jsonArray = new JSONArray(response.body().string());
-
-		// Extract the first JSONObject from the JSONArray
-		jsonObject = jsonArray.getJSONObject(0);
-
-		// Get the response body as a JSON object
-		responseBody = jsonObject.toString();
-		jsonResponse  = new JSONObject(responseBody);
-
-		// Extract the value
-		verasID = jsonResponse.getString("id");
-		System.out.println("This is the VerasID " + verasID);
-		int employeeId = jsonResponse.getInt("employeeId");
-
 		// Verifying correct account for Vera
 		assertEquals("Expected ID in Account",verasEmployeeId, employeeId);
 
@@ -95,96 +174,80 @@ public class Tests extends TestsSetup {
 	@Test
 	public void assignment2() throws InterruptedException, IOException {
 		assertTrue(serverUp);
+		verasAcc = getAccWithEmployeeId(verasEmployeeId);
+
+		// Extract the value
+		verasID = verasAcc.getString("id");
+		JSONArray verasgroups = getMemberGroupRelationWithId(verasID);
 		/**
 		 * TODO: Add code to solve the second assignment where we expect the number of
 		 * groups to be 3.
 		 */
-		System.out.println("NOW RUNNING ASSIGNMENT2!");
-		// Fetch Veras ID
-		param = "?filter=employeeId=" + verasEmployeeId;
-		URL = apiURL + "accounts" + param;
-
-		//Send a request
-		request = new Request.Builder()
-				.url(URL)
-				.method("GET", null)
-				.addHeader("Authorization", "Bearer " + token)
-				.build();
-		response = client.newCall(request).execute();
-
-		//check HTTP response status codes
-		status = response.code();
-		assertEquals(200, status);
-
-		// Parse the JSON string as a JSONArray
-		jsonArray = new JSONArray(response.body().string());
-
-		// Extract the first JSONObject from the JSONArray
-		jsonObject = jsonArray.getJSONObject(0);
-
-		// Get the response body as a JSON object
-		responseBody = jsonObject.toString();
-		jsonResponse = new JSONObject(responseBody);
-
-		// Extract the value
-		verasID = jsonResponse.getString("id");
-		int employeeId = jsonResponse.getInt("employeeId");
-
-		// Verifying correct account for Vera
-		assertEquals(verasEmployeeId, employeeId);
-
-		param = "?filter=memberId=" + verasID;
-		System.out.println("This is the param " + param);
-		URL = apiURL + "relationships" + param;
-
-		request = new Request.Builder()
-				.url(URL)
-				.method("GET", null)
-				.addHeader("Authorization", "Bearer " + token)
-				.build();
-		response = client.newCall(request).execute();
-		status = response.code();
-		assertEquals(200, status);
-
-		// Parse the JSON string as a JSONArray
-		jsonArray = new JSONArray(response.body().string());
-
-		System.out.println("This is the employeeId " + jsonArray.length());
-
-		int verasGroups = jsonArray.length();
-		int groupCount = 3;
-
-
-		// Assert which verifies the expected group count of 3
-		assertEquals("Expected number of groups", groupCount, verasGroups);
-
-
-		/**
-		 * TODO: Add Assert to verify the IDs of the groups found
-		 */
-
+		assertEquals("verify the number of groups to be 3",3,verasgroups.length());
 		// Extract from the JSONArray
 		try {
-			for (int i = 0; i < jsonArray.length(); i++) {
-				jsonObject = jsonArray.getJSONObject(i);
+			for (int i = 0; i < verasgroups.length(); i++) {
+				jsonObject = verasgroups.getJSONObject(i);
 				String groupId = jsonObject.getString("groupId");
 				String memberId = jsonObject.getString("memberId");
-
+				/**
+				 * TODO: Add Assert to verify the IDs of the groups found
+				 */
 				assertEquals("verify the ID of the group " + groupId , verasID, memberId);
+
+				if(memberId == verasID){
+					relationshipsObjects.add(jsonObject);
+				}
 			}
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
+
+
+
+		//
 	}
 
 	@Test
-	public void assignment3() throws InterruptedException {
+	public void assignment3() throws InterruptedException, IOException {
 		assertTrue(serverUp);
 
 		/**
 		 * TODO: Add code to solve the third assignment. Add Assert to verify the
 		 * expected number of groups. Add Assert to verify the IDs of the groups found.
 		 */
+
+		verasAcc = getAccWithEmployeeId(verasEmployeeId);
+
+		// Extract the value
+		verasID = verasAcc.getString("id");
+		JSONArray verasGroups = getMemberGroupRelationWithId(verasID);
+
+		// Extract from the JSONArray
+		try {
+			for (int i = 0; i < verasGroups.length(); i++) {
+				jsonObject = verasGroups.getJSONObject(i);
+				String groupId = jsonObject.getString("groupId");
+				String memberId = jsonObject.getString("memberId");
+				assertEquals("verify the ID of the group " + groupId , verasID, memberId);
+				relationshipsObjects.add(jsonObject);
+
+
+
+				JSONArray verasGroupsGroups = getMemberGroupRelationWithId(groupId);
+				for (int index = 0; index < verasGroupsGroups.length(); index++) {
+					JSONObject groupObject = verasGroupsGroups.getJSONObject(index);
+					String group_groupId = groupObject.getString("groupId");
+					String group_memberId = groupObject.getString("memberId");
+					assertEquals("verify the ID of the group " + group_groupId , groupId, group_memberId);
+					relationshipsObjects.add(groupObject);
+				}
+			}
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		assertEquals("Expected number of groups to be 6", 6,relationshipsObjects.size());
 	}
 
 	@Test
@@ -206,30 +269,5 @@ public class Tests extends TestsSetup {
 		 * managers requested
 		 */
 	}
-	@Before
-	public void getToken() throws InterruptedException, IOException {
 
-			URL = apiURL + "auth";
-			System.out.println("NOW RUNNING BEFORE!");
-
-			body = RequestBody.create(mediaType, "{\r\n  \"user\": \"apiUser\",\r\n  \"password\": \"apiPassword!\"\r\n}");
-			request = new Request.Builder()
-					.url(URL)
-					.method("POST", body)
-					.addHeader("Content-Type", "application/json")
-					.build();
-			response = client.newCall(request).execute();
-			status = response.code();
-
-			// Get the response body as a JSON object
-			responseBody = response.body().string();
-			jsonResponse = new JSONObject(responseBody);
-
-			// Extract the value
-			token = jsonResponse.getString("token");
-			System.out.println("This is the token" + token);
-
-			assertEquals(200, status);
-
-	}
 }
